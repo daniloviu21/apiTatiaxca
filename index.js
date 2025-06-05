d7f4647ca25f0f0872bb09b504c6c885b983f593
daniloviu21 <daniel.darkwatch@gmail.com>
2025-06-05 14:13:29 -0600

    feat: agregar catalogo de roles

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const categoriesRoutes = require('./routes/categoriesRoutes');
 const tablesRoutes = require('./routes/tablesRoutes');
 const statusRoutes = require('./routes/statusRoutes');
+const rolesRoutes = require('./routes/rolesRoutes');
 require('dotenv').config();
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use('/api', categoriesRoutes);
 app.use('/api', tablesRoutes);
 app.use('/api', statusRoutes);
+app.use('/api', rolesRoutes);
 
 const PORT = process.env.PORT || 3010;
 
