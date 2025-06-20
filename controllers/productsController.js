be17394f16c1adc11b1df38e5eab1050c2c53240
adansito10 <adandejesus200420@gmail.com>
2025-06-19 19:23:15 -0600

    fix: terminar lo del cloudinary en products

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -2,40 +2,35 @@ const Products = require('../models/productsModel');
 
 class ProductsController {
 
-  static async getAllProducts(req, res) {
+  static async getAll(req, res) {
     try {
-      const products = await Products.getProducts();
-      res.json(products);
-    } catch (e) {
-      res.status(500).json({ error: e.message });
-    }
-  }
-
-
-static async createProduct(req, res) {
-    try {
-      const imageUrl = req.file?.path; 
-      const data = { ...req.body,imagen: imageUrl || null
-      };
-      const product = await Products.create(data);
-      res.status(201).json(product);
-    } catch (e) {
-      res.status(500).json({ error: e.message });
-    }
-  }
-
-  static async updateProduct(req, res) {
-    try {
-      const product = await Products.update(req.params.id, req.body);
-      if (!product) return res.status(404).json({ message: 'Producto no encontrado' }
-      );
+      const product = await Products.getAll();
       res.json(product);
     } catch (e) {
       res.status(500).json({ error: e.message });
     }
   }
 
-  static async deleteProduct(req, res) {
+  static async create(req, res) {
+    try {
+      const product = await Products.create(req.body);
+      res.status(201).json(product);
+    } catch (e) {
+      res.status(500).json({ error: e.message });
+    }
+  }
+
+  static async update(req, res) {
+    try {
+      const product = await Products.update(req.params.id, req.body);
+      if (!producto) return res.status(404).json({ message: 'Producto no encontrado' });
+      res.json(product);
+    } catch (e) {
+      res.status(500).json({ error: e.message });
+    }
+  }
+
+  static async delete(req, res) {
     try {
       const product = await Products.delete(req.params.id);
       if (!product) return res.status(404).json({ message: 'Producto no encontrado' });
