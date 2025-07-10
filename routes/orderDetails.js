0705e3a7e111d1e73a838a4e4fb96cdf32df71c4
daniloviu21 <daniel.darkwatch@gmail.com>
2025-07-10 01:05:47 -0600

    fix: corregir ruta update estado

--- routes/orderDetails.js
+++ routes/orderDetails.js
@@ -6,7 +6,7 @@ router.get('/order-details', OrderDetailsController.getAll);
 router.get('/order-details/:id', OrderDetailsController.getByOrder);
 router.post('/order-details', OrderDetailsController.create);
 router.put('/order-details/:id', OrderDetailsController.update);
-router.put('/:id/estado', OrderDetailsController.updateEstado);
+router.put('/order-details/:id/estado', OrderDetailsController.updateEstado);
 router.delete('/order-details/:id', OrderDetailsController.delete);
 
 module.exports = router;
