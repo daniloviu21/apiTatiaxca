dad69422a710f5958246683bf131994409b3bf42
daniloviu21 <daniel.darkwatch@gmail.com>
2025-07-18 01:28:37 -0600

    fix: ignorar mayusculas

--- models/ingredientsModel.js
+++ models/ingredientsModel.js
@@ -28,8 +28,12 @@ class Ingredients {
         const query = `SELECT i.id, i.stock, mi.cantidad, i.nombre FROM menu_ingredientes mi JOIN ingredientes i ON mi.id_ingrediente = i.id WHERE mi.id_menu = $1`;
         const result = await client.query(query, [id_menu]);
 
+        const omitidosNormalizados = omitidos.map(n => n.trim().toLowerCase());
+
         for (const row of result.rows) {
-            if (!omitidos.includes(row.nombre)) {
+            const nombreNormalizado = row.nombre.trim().toLowerCase();
+
+            if (!omitidosNormalizados.includes(nombreNormalizado)) {
                 const cantidadDescontar = row.cantidad * cantidad;
                 await client.query(`UPDATE ingredientes SET stock = stock - $1 WHERE id = $2`,[cantidadDescontar, row.id]);
             }
