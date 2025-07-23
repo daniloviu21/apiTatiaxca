d5f99c32939628f9f1fda8aadb9ec14a0b29b018
daniloviu21 <daniel.darkwatch@gmail.com>
2025-07-22 23:47:58 -0600

    fix: cambios menores

--- routes/tablesRoutes.js
+++ routes/tablesRoutes.js
@@ -6,7 +6,7 @@ router.get('/tables', TablesController.getAllTables);
 router.get('/tables/:id', TablesController.getTableById);
 router.post('/tables', TablesController.createTable);
 router.put('/tables/:id', TablesController.updateTable);
-router.put('/mesas/:id/restore', TablesController.restoreTable);
+router.put('/tables/:id/restore', TablesController.restoreTable);
 router.delete('/tables/:id', TablesController.deleteTable);
 
 module.exports = router;
