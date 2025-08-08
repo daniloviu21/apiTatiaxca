976dc1e133a03caef5d3ff89814984d89b3a5414
daniloviu21 <daniel.darkwatch@gmail.com>
2025-08-08 14:54:35 -0600

    fix: quitar autenticación a rutas necesarias

--- routes/employeesRoutes.js
+++ routes/employeesRoutes.js
@@ -3,10 +3,10 @@ const EmployeesController = require('../controllers/employeesController');
 const router = express.Router();
 const authMiddleware = require('../config/authMiddleware');
 
-router.get('/employees', authMiddleware.authenticateToken, EmployeesController.getAllEmployees);
-router.get('/employees/:id', authMiddleware.authenticateToken, EmployeesController.getEmployeeById);
+router.get('/employees', EmployeesController.getAllEmployees);
+router.get('/employees/:id', EmployeesController.getEmployeeById);
 router.post('/employees', EmployeesController.createEmployee);
-router.put('/employees/:id', authMiddleware.authenticateToken, EmployeesController.updateEmployee);
+router.put('/employees/:id', EmployeesController.updateEmployee);
 router.delete('/employees/:id', authMiddleware.authenticateToken, EmployeesController.deleteEmployeeAndUser);
 
 module.exports = router;
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -3,10 +3,10 @@ const UsersController = require('../controllers/usersController');
 const router = express.Router();
 const authMiddleware = require('../config/authMiddleware');
 
-router.get('/users', authMiddleware.authenticateToken, UsersController.getAllUsers);
-router.get('/users/:id', authMiddleware.authenticateToken, UsersController.getUserById);
+router.get('/users', UsersController.getAllUsers);
+router.get('/users/:id', UsersController.getUserById);
 router.post('/users', UsersController.createUser);
-router.put('/users/:id', authMiddleware.authenticateToken, UsersController.updateUser);
+router.put('/users/:id', UsersController.updateUser);
 router.delete('/users/:id', authMiddleware.authenticateToken, UsersController.deleteUser);
 router.post('/users/login', UsersController.login);
 
